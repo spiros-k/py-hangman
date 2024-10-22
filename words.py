67ba1bc177a2bce6c56e420aaed35abf437a0f22
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2024-10-22 20:11:14 +0300

    add list of words

---
+++ words.py
@@ -0,0 +1,9 @@
+words_list = ["vocabulary", "reflection", "impression", "establishment", "lifestyle", 
+             "anticipation", "retirement", "initiative", "coincidence", "contradiction", 
+             "extraterrestrial", "performance", "chocolate", "partnership", "consideration", 
+             "entitlement", "imaginary", "irritating", "secretive", "competitive", 
+             "increadible", "invincible", "invisible", "imaginary", "formulate", "pronounce", 
+             "strengthen", "encounter", "reproduce", "concentrate", "challenge", "respectable", 
+             "wilderness", "articulate", "operation", "notorious", "headquarters", 
+             "premature", "announcement", "reasonable", "overwhelmed", "treasurer", 
+             "translate", "countryside", "reproduction"]
